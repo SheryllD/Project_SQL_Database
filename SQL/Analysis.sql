18e5f582019f947da602d23c657a1341eb583d56
Sheryll Dumapal <sheryllhhs@outlook.com>
2025-03-29 15:28:26 +0100

    save

--- SQL/Analysis.sql
+++ SQL/Analysis.sql
@@ -1,6 +1,6 @@
-Hypothesis 1: Can a personalised wellness app, built on behavioural patterns of healthy individuals, provide a scalable and cost-effective solution to the obesity problem in Germany?
+Problem:
+Germany faces rising weight and obesity rates due to sedentary lifestyles, poor sleep, and lack of health awareness. There is a need for personalised, data-driven solutions that encourage healthier habits in everyday life.
+Business Problem: Can a personalised wellness app, built on behavioural patterns of individuals, provide a scalable and cost-effective solution to the obesity problem in Germany?
 
-Hypothesis 2: Sufficient sleep is closely linked to an active lifestyle, contributing to overall well-being.
-
-Hypothesis 3: There is a relationship between occupations and physical activity levels, which may influence obesity rates. 
-(Sheryll: Shima you were going to change this right?)
+Hypothesis 1: Sufficient sleep is closely linked to an active lifestyle, contributing to overall well-being.
+Hypothesis 2: Physical activiy patterens vary significant by day of the week. 
